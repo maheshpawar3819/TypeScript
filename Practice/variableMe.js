3c25a7752a4a54931392b35d5e6ffcff6d08b613
maheshpawar3819 <pawarmahesh3819@gmail.com>
2025-01-03 10:47:34 +0530

    add practice files of variables

---
+++ Practice/variableMe.js
@@ -0,0 +1,9 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var id = 101;
+// id.
+var isLoggedIN = true;
+var name = "my name is mahesh";
+console.log(id);
+console.log(isLoggedIN);
+console.log(name);
---
+++ Practice/variableMe.ts
@@ -0,0 +1,13 @@
+let id:number=101;
+// id.
+
+let isLoggedIN:boolean=true;
+
+let name:string="my name is mahesh";
+
+console.log(id);
+console.log(isLoggedIN);
+console.log(name);
+
+
+export {};
